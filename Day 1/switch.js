dd28fa4540ea2497cf36dc9211531587ab112691
Aakash Prasad <73053809+aakash-prasad@users.noreply.github.com>
2022-02-01 19:38:39 +0530

    second

---
+++ Day 1/switch.js
@@ -0,0 +1,18 @@
+const shop = (grade) =>{
+  switch(grade){
+    case 'A':
+      console.log('I can travel')
+      break;
+    case 'B':
+      console.log('I can go to shopping')
+      break;
+    case 'C':
+      console.log('I can watch TV')
+      break;
+    case 'D':
+      console.log('I can go to library')
+      break;
+  }
+}
+
+shop('C');
