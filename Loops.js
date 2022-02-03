5f09b9e443eeb3be396738a8f33484d0936b0f3e
Aakash Prasad <73053809+aakash-prasad@users.noreply.github.com>
2022-02-03 18:09:32 +0530

    removed

--- Loops.js
+++
@@ -1,9 +0,0 @@
-const names = ['John', 'Spidey', 'Alex', 'Jessica', 'Martha'];
-
-// for(let i=0; i<names.length; i++){
-//   console.log(`Welcome to the channel ${names[i]}`);
-// }
-
-names.forEach((ele, index)=>{
-  console.log(`${index} Welcome to the team ${ele}`)
-})
